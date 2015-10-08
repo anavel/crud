b837ed1b356952a8b1b1ee68248b3e46f25da75e
Adrian P. Blunier <adrian@blunier.net>
2015-10-09 01:15:57 +0200

    refactoring sidebar view composer

--- src/View/Composers/SidebarComposer.php
+++ src/View/Composers/SidebarComposer.php
@@ -1,11 +1,17 @@
 <?php
 namespace ANavallaSuiza\Crudoado\View\Composers;
 
+use ANavallaSuiza\Crudoado\Contracts\Abstractor\Model as ModelAbstractor;
 use Request;
-use EasySlugger\Slugger;
 
 class SidebarComposer
 {
+    protected $abstractor;
+
+    public function __construct(ModelAbstractor $abstractor)
+    {
+        $this->abstractor = $abstractor;
+    }
 
     public function compose($view)
     {
@@ -16,16 +22,16 @@ class SidebarComposer
         $items = [];
 
         foreach ($models as $modelName => $model) {
-            $modelSlug = Slugger::slugify($modelName);
+            $this->abstractor->loadByName($modelName);
 
             $isActive = false;
-            if (strpos($url, $modelSlug) !== false) {
+            if (strpos($url, $this->abstractor->getSlug()) !== false) {
                 $isActive = true;
             }
 
             $items[] = [
-                'route' => route('crudoado.model.index', $modelSlug),
-                'name' => $modelName,
+                'route' => route('crudoado.model.index', $this->abstractor->getSlug()),
+                'name' => $this->abstractor->getName(),
                 'isActive' => $isActive
             ];
         }
