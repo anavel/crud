9bee7800f4eee97a827ca95408f39ec6a243c4df
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2015-10-08 13:33:21 +0200

    sidebar

--- src/View/Composers/SidebarComposer.php
+++ src/View/Composers/SidebarComposer.php
@@ -1,6 +1,8 @@
 <?php
 namespace ANavallaSuiza\Crudoado\View\Composers;
 
+use Request;
+
 class SidebarComposer
 {
 
@@ -8,13 +10,20 @@ class SidebarComposer
     {
         $models = config('crudoado.models');
 
+        $uri = Request::route()->uri();
+
         $items = [];
 
         foreach ($models as $modelName => $model) {
+            $isActive = false;
+            if (strpos($uri, $modelName) !== false) {
+                $isActive = true;
+            }
+
             $items[] = [
                 'route' => route('crudoado.model.index', $modelName),
                 'name' => $modelName,
-                'isActive' => false
+                'isActive' => $isActive
             ];
         }
 
