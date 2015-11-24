a2ec6cc1195d5ef3a5a70382253379c4fb08d772
Carlos Morales <carlos@anavallasuiza.com>
2015-11-24 17:31:33 +0100

    Prettier presentation

--- src/Abstractor/Eloquent/Relation/Translation.php
+++ src/Abstractor/Eloquent/Relation/Translation.php
@@ -58,7 +58,7 @@ class Translation extends Relation
 
                     $config = [
                         'name' => $this->name.'['.$key.']['.$columnName.']',
-                        'presentation' => $columnName.' '.$lang,
+                        'presentation' => ucfirst($columnName).' ['.$lang .']',
                         'form_type' => $formType,
                         'no_validate' => true,
                         'validation' => null,
