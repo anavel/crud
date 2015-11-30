8b0d0c064ab508c71c5fd0a4a646a74f3621fc55
Carlos Morales <carlos@anavallasuiza.com>
2015-11-30 13:34:37 +0100

    Translation files

--- lang/es/messages.php
+++ lang/es/messages.php
@@ -1,5 +1,26 @@
 <?php
 
 return [
-
+    'create_title'                      => 'Crear',
+    'edit_title'                        => 'Editar',
+    'show_title'                        => 'Ver',
+    'search_input'                      => 'Buscar',
+    'create_button'                     => 'Crear',
+    'empty_list'                        => 'Tabla vacía',
+    'actions_table_header'              => 'Acciones',
+    'show_button'                       => 'Ver',
+    'edit_button'                       => 'Editar',
+    'delete_button'                     => 'Borrar',
+    'back_button'                       => 'Volver',
+    'cancel_button'                     => 'Cancelar',
+    'save_button'                       => 'Guargar',
+    'confirm_button'                    => 'Confirmar',
+    'alert_success_model_store_title'   => '¡Todo bien!',
+    'alert_success_model_store_text'    => 'El elemento ha sido guardado correctamente',
+    'alert_success_model_update_title'  => '¡Todo bien!',
+    'alert_success_model_update_text'   => 'El elemento ha sido actualizado correctamente',
+    'alert_success_model_destroy_title' => '¡Todo bien!',
+    'alert_success_model_destroy_text'  => 'El elemento ha sido borrado correctamente',
+    'modal_delete_title'                => 'Estás seguro/a?',
+    'modal_delete_text'                 => 'Esto borrará permanentemente este contenido'
 ];
--- lang/gl/messages.php
+++ lang/gl/messages.php
@@ -1,5 +1,26 @@
 <?php
 
 return [
-
+    'create_title'                      => 'Crear',
+    'edit_title'                        => 'Editar',
+    'show_title'                        => 'Ver',
+    'search_input'                      => 'Buscar',
+    'create_button'                     => 'Crear',
+    'empty_list'                        => 'Táboa vacía',
+    'actions_table_header'              => 'Accións',
+    'show_button'                       => 'Ver',
+    'edit_button'                       => 'Editar',
+    'delete_button'                     => 'Borrar',
+    'back_button'                       => 'Volver',
+    'cancel_button'                     => 'Cancelar',
+    'save_button'                       => 'Gardar',
+    'confirm_button'                    => 'Confirmar',
+    'alert_success_model_store_title'   => 'Todo ben!',
+    'alert_success_model_store_text'    => 'O elemento foi gardado correctamente',
+    'alert_success_model_update_title'  => 'Todo ben!',
+    'alert_success_model_update_text'   => 'O elemento foi actualizado correctamente',
+    'alert_success_model_destroy_title' => 'Todo ben!',
+    'alert_success_model_destroy_text'  => 'O elemento foi borrado correctamente',
+    'modal_delete_title'                => 'Estás seguro/a?',
+    'modal_delete_text'                 => 'Isto borrará permanentemente este contido'
 ];
