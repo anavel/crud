d27d791ffdbd04840e57a47ca15f5683b9ebb2fe
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2016-03-23 13:16:20 +0100

    added languages

---
+++ lang/ca/messages.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'create_title'                      => 'Crear',
+    'edit_title'                        => 'Edita',
+    'show_title'                        => 'Veure',
+    'search_input'                      => 'Cercar',
+    'create_button'                     => 'Crear',
+    'empty_list'                        => 'Taula buida',
+    'actions_table_header'              => 'Accions',
+    'show_button'                       => 'Veure',
+    'edit_button'                       => 'Edita',
+    'delete_button'                     => 'Eliminar',
+    'back_button'                       => 'Tornar',
+    'cancel_button'                     => 'Cancel',
+    'save_button'                       => 'Desar',
+    'confirm_button'                    => 'Confirmar',
+    'alert_success_model_store_title'   => 'Tot bé!',
+    'alert_success_model_store_text'    => "L'element fou guardat correctament",
+    'alert_success_model_update_title'  => 'Tot bé!',
+    'alert_success_model_update_text'   => "L'element fou actualitzat correctament",
+    'alert_success_model_destroy_title' => 'Tot bé!',
+    'alert_success_model_destroy_text'  => "L'element fou eliminat correctament",
+    'modal_delete_title'                => 'Estàs segur?',
+    'modal_delete_text'                 => 'Aquesta acció esborrarà permanentment aquest contingut'
+];
