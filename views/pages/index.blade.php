47a16e710128bd031d06644295a37db1a5423e19
Lito <lito@eordes.com>
2017-09-28 22:03:48 +0200

    Improved default index template

--- views/pages/index.blade.php
+++ views/pages/index.blade.php
@@ -10,38 +10,41 @@
 <ol class="breadcrumb">
     <li><a href="{{ route('anavel-crud.home') }}"><i class="fa fa-database"></i> {{ config('anavel-crud.name') }}</a></li>
     <li class="active">{{ $abstractor->getName() }}</li>
-
 </ol>
 @stop
 
 @section('content')
 
+<?php
+$config = $abstractor->getConfig();
+$show = array_key_exists('show', $config) && ($config['show'] === true);
+$authorize = array_key_exists('authorize', $config) && ($config['authorize'] === true);
+$slug = $abstractor->getSlug();
+?>
+
 @include('anavel-crud::atoms.delete', [
-    'modelSlug' => $abstractor->getSlug(),
+    'modelSlug' => $slug,
     'modelId'   => '%id%'
 ])
 
 <div class="box">
     <div class="box-header">
-        <div class="btn-group">
-            <form method="get">
-                <div class="input-group">
-                    <input name="search" type="text" value="{{ Input::get('search') }}" class="form-control pull-right" placeholder="{{ trans('anavel-crud::messages.search_input') }}">
-                    <div class="input-group-btn">
-                        <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                    </div>
+        <div class="box-header">
+            <div class="row">
+                <div class="col-sm-9">
+                    <form method="get">
+                        <input name="search" type="text" value="{{ Input::get('search') }}" class="form-control pull-right" placeholder="{{ trans('anavel-crud::messages.search_input') }}" />
+                    </form>
                 </div>
-            </form>
-        </div>
 
-        <div class="btn-group">
-            @if (array_key_exists('authorize', $config = $abstractor->getConfig()) && $config['authorize'] === true)
-                @can('adminCreate', $abstractor->getInstance())
-                <a href="{{ route('anavel-crud.model.create', $abstractor->getSlug()) }}" class="btn btn-primary"><i class="fa fa-plus"></i> {{ trans('anavel-crud::messages.create_button') }}</a>
-                @endcan
-            @else
-                <a href="{{ route('anavel-crud.model.create', $abstractor->getSlug()) }}" class="btn btn-primary"><i class="fa fa-plus"></i> {{ trans('anavel-crud::messages.create_button') }}</a>
-            @endif
+                <div class="col-sm-1 col-sm-offset-2">
+                    @if ($authorize) @can('adminCreate', $abstractor->getInstance())
+                    <a href="{{ route('anavel-crud.model.create', $slug) }}" class="btn btn-primary"><i class="fa fa-plus"></i> {{ trans('anavel-crud::messages.create_button') }}</a>
+                    @endcan @else
+                    <a href="{{ route('anavel-crud.model.create', $slug) }}" class="btn btn-primary"><i class="fa fa-plus"></i> {{ trans('anavel-crud::messages.create_button') }}</a>
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
 
@@ -49,56 +52,49 @@
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
-                   @foreach ($abstractor->getListFields()['main'] as $field)
-                        <?php
-                        $isSorting = false;
-                        $sortDirection = 'asc';
-                        
-                        if (Input::get('sort') === $field->getName()) {
-                            $isSorting = true;
+                    <?php $sortDirection = (Input::get('direction') === 'asc') ? 'desc' : 'asc'; ?>
 
-                            if (Input::get('direction') === 'asc') {
-                                $sortDirection = 'desc';
-                            }
-                        }
-                        ?>
+                   @foreach ($abstractor->getListFields()['main'] as $field)
                     <th>
-                        <a href="{{ route('anavel-crud.model.index', [$abstractor->getSlug(), 'sort' => $field->getName(), 'direction' => $sortDirection]) }}">
+                        @if (strpos($field->getName(), '.') !== false)
+                        {{ $field->presentation() }}
+                        @else
+                        <a href="{{ route('anavel-crud.model.index', [$slug, 'sort' => $field->getName(), 'direction' => $sortDirection]) }}">
                             {{ $field->presentation() }}
                         </a>
+                        @endif
                     </th>
                     @endforeach
+
                     <th>{{ trans('anavel-crud::messages.actions_table_header') }}</th>
                 </tr>
             </thead>
+
             <tbody>
                 @forelse ($items as $item)
                 <tr>
                     @foreach ($abstractor->getListFields()['main'] as $field)
                     <td>{!! $abstractor->getFieldValue($item, $field->getName()) !!}</td>
                     @endforeach
+
                     <td>
                         {{-- Show if the model can be shown. It must be an explicit config value--}}
-                        @if (array_key_exists('show', $config = $abstractor->getConfig()) && $config['show'] === true)
-                            @can('adminView', $item)
-                            <a href="{{ route('anavel-crud.model.show', [$abstractor->getSlug(), $item->getKey()]) }}" class="btn btn-default btn-sm"><i class="fa fa-eye"></i> {{ trans('anavel-crud::messages.show_button') }}</a>
-                            @endcan
-                        @endif
+                        @if ($show) @can('adminView', $item)
+                        <a href="{{ route('anavel-crud.model.show', [$slug, $item->getKey()]) }}" class="btn btn-default btn-sm"><i class="fa fa-eye"></i> {{ trans('anavel-crud::messages.show_button') }}</a>
+                        @endcan @endif
+
                         {{-- Edit if the model has authorization and the user has permissions or if the model does not require authorization --}}
-                        @if (array_key_exists('authorize', $config = $abstractor->getConfig()) && $config['authorize'] === true)
-                            @can('adminUpdate', $item)
-                            <a href="{{ route('anavel-crud.model.edit', [$abstractor->getSlug(), $item->getKey()]) }}" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i> {{ trans('anavel-crud::messages.edit_button') }}</a>
-                            @endcan
-                        @else
-                        <a href="{{ route('anavel-crud.model.edit', [$abstractor->getSlug(), $item->getKey()]) }}" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i> {{ trans('anavel-crud::messages.edit_button') }}</a>
+                        @if ($authorize) @can('adminUpdate', $item)
+                        <a href="{{ route('anavel-crud.model.edit', [$slug, $item->getKey()]) }}" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i> {{ trans('anavel-crud::messages.edit_button') }}</a>
+                        @endcan @else
+                        <a href="{{ route('anavel-crud.model.edit', [$slug, $item->getKey()]) }}" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i> {{ trans('anavel-crud::messages.edit_button') }}</a>
                         @endif
+
                         {{-- Delete if the model has authorization and the user has permissions or if the model does not require authorization --}}
-                        @if (array_key_exists('authorize', $config = $abstractor->getConfig()) && $config['authorize'] === true)
-                            @can('adminDestroy', $item)
-                                <a href="#" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#delete-modal" data-action="{{ route('anavel-crud.model.destroy', [$abstractor->getSlug(), $item->getKey()]) }}"><i class="fa fa-trash-o"></i> {{ trans('anavel-crud::messages.delete_button') }}</a>
-                            @endcan
-                        @else
-                           <a href="#" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#delete-modal" data-action="{{ route('anavel-crud.model.destroy', [$abstractor->getSlug(), $item->getKey()]) }}"><i class="fa fa-trash-o"></i> {{ trans('anavel-crud::messages.delete_button') }}</a>
+                        @if ($authorize) @can('adminDestroy', $item)
+                        <a href="#" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#delete-modal" data-action="{{ route('anavel-crud.model.destroy', [$slug, $item->getKey()]) }}"><i class="fa fa-trash-o"></i> {{ trans('anavel-crud::messages.delete_button') }}</a>
+                        @endcan @else
+                        <a href="#" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#delete-modal" data-action="{{ route('anavel-crud.model.destroy', [$slug, $item->getKey()]) }}"><i class="fa fa-trash-o"></i> {{ trans('anavel-crud::messages.delete_button') }}</a>
                         @endif
                     </td>
                 </tr>
@@ -118,7 +114,6 @@
     @endif
 </div>
 @stop
-
 
 @section('footer-scripts')
     @parent
